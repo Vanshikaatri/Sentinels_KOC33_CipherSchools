c3d7e2f8a6cb515043fa46ad96a80c32289ad534
Vanshikaatri <118870986+Vanshikaatri@users.noreply.github.com>
2022-12-29 03:55:14 -0600

    Add files via upload

---
+++ project.py
@@ -0,0 +1,27 @@
+import random
+Title = [' Dont Giveup :-     ',' Peresistannce is the key:-    ']
+a = ['A long time ago ', 'Once upon a time ', 'Few years back ']
+b = [',there lived a boy named Robert. ',',there was a man named Jack.',',there lived a farmer.']
+c = ['He was living a calm life, ','He was very happy with his family, ','He was enjoying his life, ']
+d = ['but his happiness could not last long. ','but his calm life was soon going to be dreadful. ']
+e = ['His village faced a severe drought. ', 'His city faced a severe dry spell. ']
+f = ['They waited for the rains with no luck. ', 'They were looking at the sky with their thirsty throats. ','The people were searching dark clouds in the sky. ']
+g = ['All the crops were drying up ','All the land dried up ','All the trees dried up ']
+h = ['and the cattles started dying. ', 'and various animals faced death. ','and many people were on the verge of death. ']
+i = ['Oneday,during a meet with other people, ','One night,during a meeting, ','In the afternoon,during a meeting, ']
+j = ['he said that the only solution for drought can be groundwater. ','he was trying to convince others to dig for groundwater. ']
+k = ['They agreed with him. ','Some agreed with him. ']
+l = ['They all started digging for the groundwater. ','Some accompanied with him for digging for the hunt of groundwater. ']
+m = ['Many of them gave up pretty easily. ','Some of them gave up. ','All of them gave up except him. ']
+n = ['He still kept on going, ','He never gave up, ','He relentlessly worked hard, ']
+o = ['though he was criticised. ','though he was told by other people to give up. ']
+p = ['Atlast after many days, ','One day, ','One night, ']
+q = ['he saw water. ','he saw groundwater. ','he sighted life which is water. ']
+r = ['He advised others that, ','He told others that, ','He made people understand that, ']
+s = ['never giveup so easily. ','never lose hopes so eaily. ','have faith in whatever you do. ']
+t = ['Now they had plenty of water. ','Now they never fell short of water. ']
+u = ['Moral:-Perseverance paves way to success. ','Moral:-Work relentlessly to achieve success. ','Moral:-Hard work and no give up attitude is the key toward success. ']
+print(random.choice(Title)+random.choice(a)+random.choice(b)+random.choice(c)+
+random.choice(d) +random.choice(e)+random.choice(f)+random.choice(g)+random.choice(h)+
+random.choice(i)+random.choice(j)+random.choice(k)+random.choice(l)+random.choice(m)+random.choice(n)+
+random.choice(o)+random.choice(p)+random.choice(q)+random.choice(r)+random.choice(s)+random.choice(t)+random.choice(u))
